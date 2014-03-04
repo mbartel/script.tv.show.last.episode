7f0a71b9790e70ff92a3e0fc25f89f893e3145ac
mbartel <mbartel@roamsys.com>
2014-03-04 14:39:57 +0100

    bugfixes

--- default.py
+++ default.py
@@ -1,12 +1,12 @@
-import os.path, sqlite3
-import os, sys, re, socket, urllib, unicodedata, threading, urlparse
+import sqlite3
+import sys, unicodedata, urlparse
 import xbmc, xbmcgui, xbmcaddon, xbmcvfs, xbmcplugin
 
 
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
- 
+
 xbmcplugin.setContent(addon_handle, 'movies')
 
 __addon__     = xbmcaddon.Addon()
@@ -17,37 +17,38 @@ __version__   = __addon__.getAddonInfo('version')
 __language__  = __addon__.getLocalizedString
 
 
-def displayEpisodeList(episodeList):
-  for episode in episodeList:
-    label = u"[%s] - S%02dE%02d - %s"%(episode['firstAired'], int(episode['season']), int(episode['episode']), episode['title'])
+def display_episode_list(seriesList):
+  for series in seriesList:
+    episode = series['episode']
+    label = u"S%02dE%02d - %s (%s, %s)"%(int(series['season']), int(episode['number']), series['title'], episode['title'], episode['firstAired'])
     li = xbmcgui.ListItem(label, iconImage='DefaultFolder.png')
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=base_url, listitem=li, isFolder=False)
   xbmcplugin.endOfDirectory(addon_handle)
 
-def getDatabaseFile():
+def get_database_file():
   folder, files = xbmcvfs.listdir('special://database/')
   for file in files:
     if 'MyVideos' in file:
       return xbmc.translatePath('special://database/' + file)
 
-def getLastEpisode(conn, idShow, season):
-  episodeQuery = 'SELECT max(c13) FROM episodeview WHERE idShow = ? AND c12 = ?'
+def get_last_episode(conn, idShow, season):
+  episodeQuery = 'SELECT MAX(CAST(c13 AS INTEGER)), c00, c05 FROM episodeview WHERE idShow = ? AND c12 = ?'
   cursor = conn.cursor()
   cursor.execute(episodeQuery, (idShow, season))
-  return cursor.fetchone()[0]
-  
+  episode = cursor.fetchone()
+  return { 'number': episode[0], 'title': episode[1], 'firstAired': episode[2] }
 
-def getTVShowList():
-  tvshowQuery = 'SELECT idShow, strTitle, max(c12), c05 FROM episodeview GROUP BY strTitle ORDER BY strTitle'
-  conn = sqlite3.connect(getDatabaseFile())
+
+def get_tv_show_list():
+  tvshowQuery = 'SELECT idShow, strTitle, MAX(CAST(c12 AS INTEGER)) FROM episodeview GROUP BY idShow ORDER BY strTitle'
+  conn = sqlite3.connect(get_database_file())
   cursor = conn.cursor()
   tvshows = cursor.execute(tvshowQuery)
 
   episodeList = []
   for tvshow in tvshows:
-    episodeList.append({ 'title': tvshow[1], 'season': tvshow[2], 'firstAired': tvshow[3],
-               'episode': getLastEpisode(conn, tvshow[0], tvshow[2]) })
+    episodeList.append({ 'title': tvshow[1], 'season': tvshow[2], 'episode': get_last_episode(conn, tvshow[0], tvshow[2]) })
   return episodeList
-    
-displayEpisodeList(getTVShowList())
+
+displayEpisodeList(get_tv_show_list())
 
